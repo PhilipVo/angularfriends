e91ef4833458e127f1a9326959275c999f276ad2
Philip Vo <philiptranbavo@gmail.com>
2016-10-19 21:06:17 -0700

    Removed comments.

--- client/assets/controllers/editController.js
+++ client/assets/controllers/editController.js
@@ -1,9 +1,7 @@
 app.controller('editController', function ($scope, $location, $routeParams, friendsFactory) {
 	friendsFactory.show($routeParams.id, function(friend) {
-		console.log("type is:", typeof(friend.birthday))
 		friend.birthday = new Date(friend.birthday)
 		$scope.friend = friend;
-		console.log('here')
 	});
 
 	$scope.update = function() {
